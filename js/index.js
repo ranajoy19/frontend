8efb54eb53cc0dfb773cc6359159281676fad744
ranajoy <ranajoybhattacharjee19@gmail.com>
2023-07-18 21:51:24 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.querySelector('h1').innerHTML = 'hey its me';
+
+
+
+// document.getElementById("h").innerHTML ="BULBUL";
+
+// document.querySelector("#h").classList.add("invisible");
+// document.querySelector("#h").classList.toggle("invisible");
+// document.querySelector("#title")
+// document.querySelector("#h").innerHTML="<em>Rani<em>";
+//document.querySelector("#h").getAttributeNames;
